9d27f58eca91adf2ea224232932fa07f967e6bcd
Xander Low <alexanderjameslow@gmail.com>
2022-10-04 18:10:57 -0700

    added: top-level scripts for dev

--- packages/react-p5/vite.config.ts
+++ packages/react-p5/vite.config.ts
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react"
 import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
       name: "react-p5",
-      formats: ["es", "umd", "cjs"],
+      formats: ["es", "umd"],
       fileName: format => `react-p5.${format}.js`,
     },
     rollupOptions: {
@@ -19,10 +19,12 @@ export default defineConfig({
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-          p5: "p5",
+          p5: "P5",
         },
         exports: "named",
       },
     },
   },
 })
+
+export default config
